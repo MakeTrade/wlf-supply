8e033a2a79908527c4b10b199d360e8ddc037b81
MakeTrade <44473011+MakeTrade@users.noreply.github.com>
2024-10-16 02:00:46 +0100

    add files

---
+++ api/circulating-supply.ts
@@ -0,0 +1,28 @@
+import type { VercelRequest, VercelResponse } from '@vercel/node'
+import { ethers, BigNumber } from 'ethers'
+import dotenv from 'dotenv'
+import ERC20 from '../abis/ERC20.json'
+
+dotenv.config()
+
+export default async (request: VercelRequest, response: VercelResponse) => {
+  const provider = new ethers.providers.JsonRpcProvider('https://rpc.ankr.com/eth')
+
+  const nationContract = new ethers.Contract(
+    process.env.NATION_ADDRESS as string,
+    ERC20.abi,
+    provider
+  )
+
+  const supplies: BigNumber[] = await Promise.all([
+    nationContract.totalSupply(),
+    nationContract.balanceOf(process.env.TREASURY_ADDRESS),
+    nationContract.balanceOf(process.env.VENATION_ADDRESS),
+  ])
+
+  const [totalSupply, treasurySupply, veNationSupply] = supplies
+
+  const circulatingSupply = totalSupply.sub(treasurySupply).sub(veNationSupply)
+
+  response.status(200).send(ethers.utils.formatUnits(circulatingSupply, 9))
+}
---
+++ api/total-supply.ts
@@ -0,0 +1,20 @@
+import type { VercelRequest, VercelResponse } from '@vercel/node'
+import { ethers, BigNumber } from 'ethers'
+import dotenv from 'dotenv'
+import ERC20 from '../abis/ERC20.json'
+
+dotenv.config()
+
+export default async (request: VercelRequest, response: VercelResponse) => {
+  const provider = new ethers.providers.JsonRpcProvider('https://rpc.ankr.com/eth')
+
+  const nationContract = new ethers.Contract(
+    process.env.NATION_ADDRESS as string,
+    ERC20.abi,
+    provider
+  )
+
+  const totalSupply: BigNumber = await nationContract.totalSupply()
+
+  response.status(200).send(ethers.utils.formatUnits(totalSupply, 9))
+}
